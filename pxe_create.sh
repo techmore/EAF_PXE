9bb3013b2959d24e9f678a01d2ca5f625b69f496
techmore <sdolbec1@gmail.com>
2015-07-23 16:49:07 -0400

    Update pxe_create.sh

--- pxe_create.sh
+++ pxe_create.sh
@@ -28,16 +28,33 @@ if [ ! -f ~/Downloads/clonezilla-live-2.4.2-10-i586.iso ]; then
 else
    echo "clonezilla-live-2.4.2-10-i586.iso found."
 fi
+
+
 /*
+
+Edubuntu 	A complete Linux based operating system targeted for primary and secondary education. It is freely available with community based support. The Edubuntu community is built on the ideas enshrined in the Edubuntu Manifesto: that software, especially for education, should be available free of charge and that software tools should be usable by people in their local language and despite any disabilities.[54]
+Kubuntu 	An official derivative of Ubuntu Linux using KDE instead of the GNOME or Unity interfaces used by default in Ubuntu.[55]
+Ubuntu GNOME 	An official Ubuntu variant[56] that uses Gnome Shell as its default desktop and GDM as its display manager.[57]
+Ubuntu MATE 	An official derivative of Ubuntu using MATE, a desktop environment forked from the now-defunct GNOME 2 code base, with an emphasis on the desktop metaphor.[58]
+Lubuntu 	Lubuntu is a project that is an official derivative of the Ubuntu operating system that is "lighter, less resource hungry and more energy-efficient", using the LXDE desktop environment.[59][60][61]
+Xubuntu 	An official derivative of Ubuntu using Xfce. Xubuntu is intended for use on less-powerful computers or those who seek a highly efficient desktop environment on faster systems, and uses mostly GTK+ applications.[62]
+Mythbuntu 	Based on Ubuntu and MythTV, providing applications for recording TV and acting as a media center.[63]
+Ubuntu for Android 	Designed for use with Android phones.[64]
+UbuntuKylin 	An official derivative aimed at the Chinese market.
+Ubuntu Server 	An official derivative made for use in servers. Ubuntu Server handles mail, controls printers, acts as a fileserver, can host LAMP and more.[65]
+Ubuntu Studio 	Based on Ubuntu, providing open-source applications for multimedia creation aimed at the audio, video and graphic editors.[66]
+Ubuntu Touch 	Designed for use with touchscreen devices.
+Ubuntu TV 	Designed for use with TVs.[67]
+
+
 Desktop
 http://releases.ubuntu.com/14.04.2/ubuntu-14.04.2-desktop-amd64.iso.torrent
 http://releases.ubuntu.com/14.04.2/ubuntu-14.04.2-desktop-i386.iso.torrent
 Server
 http://releases.ubuntu.com/14.04.2/ubuntu-14.04.2-server-amd64.iso.torrent
 http://releases.ubuntu.com/14.04.2/ubuntu-14.04.2-server-i386.iso.torrent
-Net install 64bit
+Net install
 http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/netboot.tar.gz
-Net install 32bit
 http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-i386/current/images/netboot/netboot.tar.gz
 
 Edubuntu
@@ -47,6 +64,21 @@ http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.2-dvd
 Lubuntu
 http://cdimage.ubuntu.com/lubuntu/releases/14.04.2/release/lubuntu-14.04.2-desktop-i386.iso.torrent
 http://cdimage.ubuntu.com/lubuntu/releases/14.04.2/release/lubuntu-14.04.2-desktop-amd64.iso.torrent
+
+Kubuntu
+http://cdimage.ubuntu.com/kubuntu/releases/14.04.2/release/kubuntu-14.04.2-desktop-amd64.iso.torrent
+http://cdimage.ubuntu.com/kubuntu/releases/14.04.2/release/kubuntu-14.04.2-desktop-i386.iso.torrent
+
+
+
+
+Ubuntu-NEXT
+http://cdimage.ubuntu.com/ubuntu-desktop-next/backup-20150422/vivid-desktop-amd64.iso
+http://cdimage.ubuntu.com/ubuntu-desktop-next/backup-20150422/vivid-desktop-i386.iso
+
+ubuntmyth
+http://cdimage.ubuntu.com/mythbuntu/releases/14.04.2/release/mythbuntu-14.04.2-desktop-amd64.iso.torrent
+http://cdimage.ubuntu.com/mythbuntu/releases/14.04.2/release/mythbuntu-14.04.2-desktop-i386.iso.torrent
 
 ubuntustudio
 http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.2/release/ubuntustudio-14.04.2-dvd-i386.iso.torrent
