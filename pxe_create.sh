34ee72c83d385563c65b885c01e60823f0dca22b
techmore <sdolbec1@gmail.com>
2015-07-23 16:20:26 -0400

    Update pxe_create.sh

--- pxe_create.sh
+++ pxe_create.sh
@@ -104,7 +104,7 @@ sudo cp /mnt/loop/casper/vmlinuz /var/lib/tftpboot/ubuntu-14.04.2-desktop-i386
 sudo cp /mnt/loop/casper/initrd.lz /var/lib/tftpboot/ubuntu-14.04.2-desktop-i386
 
 if [ ! -d /srv/install/ubuntu-14.04.2-desktop-i386 ]; then
-  sudo mkdir /srv/install/ubuntu-14.04.2-desktop-i386; fi
+  sudo mkdir -p /srv/install/ubuntu-14.04.2-desktop-i386; fi
 sudo cp -R /mnt/loop/* /srv/install/ubuntu-14.04.2-desktop-i386
 sudo cp -R /mnt/loop/.disk /srv/install/ubuntu-14.04.2-desktop-i386
 sudo umount /mnt/loop
