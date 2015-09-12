f94ad812630ed159edb12aa80b95289a3acaac03
techmore <sdolbec1@gmail.com>
2015-09-12 00:51:15 -0400

    Update add_Ubuntu_Family.sh

--- add_Ubuntu_Family.sh
+++ add_Ubuntu_Family.sh
@@ -12,48 +12,51 @@ Add These x86 / x64
     http://cdimage.ubuntu.com/kubuntu/releases/trusty/release/kubuntu-14.04.3-desktop-i386.iso.torrent
     http://cdimage.ubuntu.com/kubuntu/releases/trusty/release/kubuntu-14.04.3-desktop-amd64.iso.torrent
  Net Install
+http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-i386/current/images/netboot/netboot.tar.gz
+http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/netboot.tar.gz
  Ubuntu
+ http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso
+ http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso.torrent
+ http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso
+ http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso.torrent
  
- Ubuntu Server 	An official derivative made for use in servers. Ubuntu Server handles mail, controls printers, acts as a fileserver, can host LAMP and more.
+ Ubuntu Server -	An official derivative made for use in servers. Ubuntu Server handles mail, controls printers, acts as a fileserver, can host LAMP and more.
+    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-i386.iso.torrent
+    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-amd64.iso.torrent
  Ubuntu GNOME 	An official Ubuntu variant[56] that uses Gnome Shell as its default desktop and GDM as its display manager.
    http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-i386.iso
    http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-i386.iso.torrent
    http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-amd64.iso
    http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-amd64.iso.torrent
  
- Ubuntu MATE 	An official derivative of Ubuntu using MATE, a desktop environment forked from the now-defunct GNOME 2 code base, with an emphasis on the desktop metaphor.
+ Ubuntu MATE -	An official derivative of Ubuntu using MATE, a desktop environment forked from the now-defunct GNOME 2 code base, with an emphasis on the desktop metaphor.
      http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-i386.iso
      http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-i386.iso.torrent
      http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-amd64.iso
      http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-amd64.iso.torrent
 
- Lubuntu 	Lubuntu is a project that is an official derivative of the Ubuntu operating system that is "lighter, less resource hungry and more energy-efficient", using the LXDE desktop environment.
+ Lubuntu -	Lubuntu is a project that is an official derivative of the Ubuntu operating system that is "lighter, less resource hungry and more energy-efficient", using the LXDE desktop environment.
      http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-i386.iso
      http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-i386.iso.torrent
      http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-amd64.iso
      http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-amd64.iso.torrent
      
- Xubuntu 	An official derivative of Ubuntu using Xfce. Xubuntu is intended for use on less-powerful computers or those who seek a highly efficient desktop environment on faster systems, and uses mostly GTK+ applications.
+ Xubuntu -	An official derivative of Ubuntu using Xfce. Xubuntu is intended for use on less-powerful computers or those who seek a highly efficient desktop environment on faster systems, and uses mostly GTK+ applications.
     http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-i386.iso
     http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-i386.iso.torrent
     http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-amd64.iso
     http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-amd64.iso.torrent
     
- Mythbuntu 	Based on Ubuntu and MythTV, providing applications for recording TV and acting as a media center.
+ Mythbuntu -	Based on Ubuntu and MythTV, providing applications for recording TV and acting as a media center.
      http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-i386.iso
      http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-i386.iso.torrent
      http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-amd64.iso
      http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-amd64.iso.torrent
      
- Ubuntu Studio 	Based on Ubuntu, providing open-source applications for multimedia creation aimed at the audio, video and graphic editors.
+ Ubuntu Studio -	Based on Ubuntu, providing open-source applications for multimedia creation aimed at the audio, video and graphic editors.
      http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-i386.iso
      http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-i386.iso.torrent
      http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-amd64.iso
      http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-amd64.iso.torrent
 
 */
-
-http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso.torrent
-http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso.torrent
-http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-i386.iso.torrent
-http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-amd64.iso.torrent
