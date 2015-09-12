bfe0cf87bbcaad1987d5ea2ed0494f868d0f384c
techmore <sdolbec1@gmail.com>
2015-09-11 22:31:47 -0400

    Update pxe_create.sh

--- pxe_create.sh
+++ pxe_create.sh
@@ -80,22 +80,6 @@ else
    echo "clonezilla-live-2.4.2-10-i586.iso found."
 fi
 
-
-/*
-Add These x86 / x64
- Edubuntu 	A complete Linux based operating system targeted for primary and secondary education. It is freely available with community based support. The Edubuntu community is built on the ideas enshrined in the Edubuntu Manifesto: that software, especially for education, should be available free of charge and that software tools should be usable by people in their local language and despite any disabilities.[54]
- Kubuntu 	An official derivative of Ubuntu Linux using KDE instead of the GNOME or Unity interfaces used by default in Ubuntu.[55]
- Net Install
- Ubuntu
- Ubuntu GNOME 	An official Ubuntu variant[56] that uses Gnome Shell as its default desktop and GDM as its display manager.[57]
- Ubuntu MATE 	An official derivative of Ubuntu using MATE, a desktop environment forked from the now-defunct GNOME 2 code base, with an emphasis on the desktop metaphor.[58]
- Lubuntu 	Lubuntu is a project that is an official derivative of the Ubuntu operating system that is "lighter, less resource hungry and more energy-efficient", using the LXDE desktop environment.[59][60][61]
- Xubuntu 	An official derivative of Ubuntu using Xfce. Xubuntu is intended for use on less-powerful computers or those who seek a highly efficient desktop environment on faster systems, and uses mostly GTK+ applications.[62]
- Mythbuntu 	Based on Ubuntu and MythTV, providing applications for recording TV and acting as a media center.[63]
- Ubuntu Server 	An official derivative made for use in servers. Ubuntu Server handles mail, controls printers, acts as a fileserver, can host LAMP and more.[65]
- Ubuntu Studio 	Based on Ubuntu, providing open-source applications for multimedia creation aimed at the audio, video and graphic editors.[66]
-
-*/
 echo ""
 if [ ! -f ~/Downloads/ubuntu-14.04.3-desktop-i386.iso ]; then
    echo "ubuntu/ubuntu-14.04.3-desktop-i386.iso NOT found, attempting to download."
