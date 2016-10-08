79fe973f727b0fc61928be043023d1b966f2f546
techmore <sdolbec1@gmail.com>
2016-10-08 14:10:44 -0400

    Update add_Ubuntu_Family.sh

--- add_Ubuntu_Family.sh
+++ add_Ubuntu_Family.sh
@@ -2,48 +2,52 @@
 
 #  /srv/install 23GB
 echo "This script will install x86 / x64 versions of the following operating systems. 
- Edu buntu - A complete Linux based operating system targeted for primary and secondary education. It is freely available with community based support. The Edubuntu community is built on the ideas enshrined in the Edubuntu Manifesto: that software, especially for education, should be available free of charge and that software tools should be usable by people in their local language and despite any disabilities.
-Kubuntu - An official derivative of Ubuntu Linux using KDE instead of the GNOME or Unity interfaces used by default in Ubuntu.
+ Edubuntu - A complete Linux based operating system targeted for primary and secondary education. It is freely available with community based support. The Edubuntu community is built on the ideas enshrined in the Edubuntu Manifesto: that software, especially for education, should be available free of charge and that software tools should be usable by people in their local language and despite any disabilities.
  Net Installer
  Ubuntu Desktop 
  Ubuntu Server - An official derivative made for use in servers. Ubuntu Server handles mail, controls printers, acts as a fileserver, can host LAMP and more.
- Ubuntu GNOME - An official Ubuntu variant[56] that uses Gnome Shell as its default desktop and GDM as its display manager.
- Ubuntu MATE - An official derivative of Ubuntu using MATE, a desktop environment forked from the now-defunct GNOME 2 code base, with an emphasis on the desktop metaphor.
  Lubuntu - Lubuntu is a project that is an official derivative of the Ubuntu operating system that is lighter, less resource hungry and more energy-efficient, using the LXDE desktop environment.
  Xubuntu - An official derivative of Ubuntu using Xfce. Xubuntu is intended for use on less-powerful computers or those who seek a highly efficient desktop environment on faster systems, and uses mostly GTK+ applications.
  Mythbuntu - Based on Ubuntu and MythTV, providing applications for recording TV and acting as a media center.
   Ubuntu Studio - Based on Ubuntu, providing open-source applications for multimedia creation aimed at the audio, video and graphic editors. "
 
 ubuntu_array=(
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-amd64.iso
+#   http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso
+#   http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso
+#   http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-amd64.iso
+    http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-desktop-amd64.iso
+    http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-server-amd64.iso
 )
 ubuntu_net_array=(
-    http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-i386/current/images/netboot/netboot.tar.gz
-    http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/netboot.tar.gz
+#   http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-i386/current/images/netboot/netboot.tar.gz
+#   http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/netboot.tar.gz
+    http://archive.ubuntu.com/ubuntu/dists/xenial-updates/main/installer-amd64/current/images/netboot/netboot.tar.gz
 )
 iso_array=(
-   http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-amd64.iso 
-   http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-i386.iso 
-     
-   http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-i386.iso
-   http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-amd64.iso
+#  http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-amd64.iso 
+#  http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-i386.iso 
+   http://cdimage.ubuntu.com/edubuntu/releases/14.04.5/release/edubuntu-14.04.5-dvd-i386.iso
 
-   http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-amd64.iso
+#   http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-i386.iso
+#   http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-amd64.iso
+
+#   http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-i386.iso
+#   http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-amd64.iso
     
-   http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-amd64.iso
+#   http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-i386.iso
+#   http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-amd64.iso
+    http://cdimage.ubuntu.com/lubuntu/xenial/daily/current/xenial-alternate-amd64.iso
 
-   http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-amd64.iso
+#  http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-i386.iso
+#  http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-amd64.iso
 
-   http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-amd64.iso
-     
-   http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-i386.iso
-   http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-amd64.iso
+#  http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-i386.iso
+#  http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-amd64.iso
+   http://cdimage.ubuntu.com/mythbuntu/xenial/daily-live/current/xenial-desktop-amd64.iso
+
+#   http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-i386.iso
+#   http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-amd64.iso
+   http://cdimage.ubuntu.com/ubuntustudio/xenial/dvd/current/xenial-dvd-amd64.iso
 )
 
 torrent_array=(
