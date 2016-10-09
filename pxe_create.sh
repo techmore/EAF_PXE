9fba5780209d41e080894e79563a3c64d44aacab
techmore <sdolbec1@gmail.com>
2016-10-09 09:47:02 -0400

    Update pxe_create.sh

--- pxe_create.sh
+++ pxe_create.sh
@@ -78,50 +78,50 @@ guest ok = yes
 EOF
 
 sudo service smbd restart
-/*
+
 # 7/23/15 DBan has been updated  if [ ! -f ~/Downloads/dban-2.2.8_i586.iso ]; then
 # http://sourceforge.net/projects/dban/files/dban/dban-2.3.0/dban-2.3.0_i586.iso
-if [ ! -f /home/user/Downloads/dban-2.3.0_i586.iso ]; then
-   echo "~/Downloads/dban-2.3.0_i586.iso NOT found, attempting to download."
-   echo "--Downloading dban-2.3.0_i586.iso..."
-   cd /var/www/html/4.ISOs
-   wget http://downloads.sourceforge.net/project/dban/dban/dban-2.3.0/dban-2.3.0_i586.iso
-   echo "DBAN download complete."
-   echo ""
-else
-   echo "dban-2.3.0_i586.iso found."
-   mv /home/user/Downloads/dban-2.3.0_i586.iso /var/www/html/4.ISOs 
-fi
-echo ""
+
+# if [ ! -f /home/user/Downloads/dban-2.3.0_i586.iso ]; then
+#   echo "~/Downloads/dban-2.3.0_i586.iso NOT found, attempting to download."
+#   echo "--Downloading dban-2.3.0_i586.iso..."
+#   cd /var/www/html/4.ISOs
+#   wget http://downloads.sourceforge.net/project/dban/dban/dban-2.3.0/dban-2.3.0_i586.iso
+#   echo "DBAN download complete."
+#   echo ""
+# else
+#   echo "dban-2.3.0_i586.iso found."
+#   mv /home/user/Downloads/dban-2.3.0_i586.iso /var/www/html/4.ISOs 
+# fi
+# echo ""
 
 # 7/23/15 Clonezilla version updated
-if [ ! -f /home/user/Downloads/clonezilla-live-2.4.7-8-amd64.iso ]; then
-   echo "clonezilla-live-2.4.7-8-amd64.iso NOT found, attempting to download."
-   echo "--Downloading clonezilla-live-2.4.7-8-amd64.iso ..."
-   cd /var/www/html/4.ISOs
+#if [ ! -f /home/user/Downloads/clonezilla-live-2.4.7-8-amd64.iso ]; then
+#   echo "clonezilla-live-2.4.7-8-amd64.iso NOT found, attempting to download."
+#   echo "--Downloading clonezilla-live-2.4.7-8-amd64.iso ..."
+#   cd /var/www/html/4.ISOs
 #   wget http://downloads.sourceforge.net/project/clonezilla/clonezilla_live_stable/2.4.2-10/clonezilla-live-2.4.2-10-i586.iso
 # 7/23/16 Clonezilla update
-   wget https://osdn.jp/frs/redir.php?m=gigenet&f=%2Fclonezilla%2F66042%2Fclonezilla-live-2.4.7-8-amd64.iso
-else
-   echo "clonezilla-live-2.4.7-8-amd64.iso found."
-   mv /home/user/Downloads/clonezilla-live-2.4.7-8-amd64.iso /var/www/html/4.ISOs
-fi
+#   wget https://osdn.jp/frs/redir.php?m=gigenet&f=%2Fclonezilla%2F66042%2Fclonezilla-live-2.4.7-8-amd64.iso
+#else
+#   echo "clonezilla-live-2.4.7-8-amd64.iso found."
+#   mv /home/user/Downloads/clonezilla-live-2.4.7-8-amd64.iso /var/www/html/4.ISOs
+#fi
 
-echo ""
-if [ ! -f /home/user/Downloads/ubuntu-16.04.1-desktop-amd64.iso ]; then
-   echo "ubuntu-16.04.1-desktop-amd64.iso NOT found, attempting to download."
-   echo "--Downloading ubuntu-16.04.1-desktop-amd64.iso..."
-   cd /var/www/html/4.ISOs
+#echo ""
+#if [ ! -f /home/user/Downloads/ubuntu-16.04.1-desktop-amd64.iso ]; then
+#   echo "ubuntu-16.04.1-desktop-amd64.iso NOT found, attempting to download."
+#   echo "--Downloading ubuntu-16.04.1-desktop-amd64.iso..."
+#   cd /var/www/html/4.ISOs
 #  wget  https://nyc3.dl.elementary.io/download/MTQ0MjE4OTk5Nw==/elementaryos-stable-0.3.1-i386.20150903.iso
 #  https://nyc3.dl.elementary.io/download/MTQ3NDg1NjE4MA==/elementaryos-0.4-stable-amd64.20160921.iso
 
 #   wget http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso
-   wget http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-desktop-amd64.iso
-else
-   echo "ubuntu-16.04.1-desktop-amd64.iso found."
-   cp /home/user/Downloads/ubuntu-16.04.1-desktop-amd64.iso /var/www/html/4.ISOs
-fi
-*/
+#   wget http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-desktop-amd64.iso
+#else
+#   echo "ubuntu-16.04.1-desktop-amd64.iso found."
+#   cp /home/user/Downloads/ubuntu-16.04.1-desktop-amd64.iso /var/www/html/4.ISOs
+#fi
 
 echo ""
 if [ ! -f /home/user/Downloads/netboot.tar.gz ]; then
@@ -140,80 +140,80 @@ chown -R nobody:nogroup /var/lib/tftpboot
 # https://nyc3.dl.elementary.io/download/MTQ0MjE4OTk5Nw==/elementaryos-stable-0.3.1-i386.20150903.iso
 # https://nyc3.dl.elementary.io/download/MTQ0MjE4OTk5Nw==/elementaryos-stable-0.3.1-amd64.20150903.iso
 # Sperated to be readable
-/*
-   mkdir -p /mnt/loop /srv/install
-   mkdir -p /srv/install/ubuntu-16.04.1-desktop-amd64
-   mkdir -p /var/lib/tftpboot/ubuntu-16.04.1-desktop-amd64
-   mkdir -p /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64 
-   mkdir -p /var/lib/tftpboot/dban-2.3.0_i586 /srv/install/dban-2.3.0_i586 
 
-mount -o loop -t iso9660 /var/www/html/4.ISOs/dban-2.3.0_i586.iso /mnt/loop
-cp /mnt/loop/dban.bzi /var/lib/tftpboot/dban-2.3.0_i586/dban.bzi
-umount /mnt/loop
+#   mkdir -p /mnt/loop /srv/install
+#   mkdir -p /srv/install/ubuntu-16.04.1-desktop-amd64
+#   mkdir -p /var/lib/tftpboot/ubuntu-16.04.1-desktop-amd64
+#   mkdir -p /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64 
+#   mkdir -p /var/lib/tftpboot/dban-2.3.0_i586 /srv/install/dban-2.3.0_i586 
 
-mount -o loop -t iso9660 /var/www/html/4.ISOs/clonezilla-live-2.4.7-8-amd64.iso /mnt/loop
-cp /mnt/loop/live/vmlinuz /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64
-cp /mnt/loop/live/initrd.img /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64
-cp /mnt/loop/live/filesystem.squashfs /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64
-umount /mnt/loop
+# mount -o loop -t iso9660 /var/www/html/4.ISOs/dban-2.3.0_i586.iso /mnt/loop
+# cp /mnt/loop/dban.bzi /var/lib/tftpboot/dban-2.3.0_i586/dban.bzi
+# umount /mnt/loop
 
-mount -o loop -t iso9660 /var/www/html/4.ISOs/ubuntu-16.04.1-desktop-amd64.iso /mnt/loop
-cp /mnt/loop/casper/vmlinuz.efi /var/lib/tftpboot/ubuntu-16.04.1-desktop-amd64
-cp /mnt/loop/casper/initrd.lz /var/lib/tftpboot/ubuntu-16.04.1-desktop-amd64
-cp -R /mnt/loop/* /srv/install/ubuntu-16.04.1-desktop-amd64
-cp -R /mnt/loop/.disk /srv/install/ubuntu-16.04.1-desktop-amd64
-umount /mnt/loop
+# mount -o loop -t iso9660 /var/www/html/4.ISOs/clonezilla-live-2.4.7-8-amd64.iso /mnt/loop
+# cp /mnt/loop/live/vmlinuz /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64
+# cp /mnt/loop/live/initrd.img /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64
+# cp /mnt/loop/live/filesystem.squashfs /var/lib/tftpboot/clonezilla-live-2.4.7-8-amd64
+# umount /mnt/loop
+
+# mount -o loop -t iso9660 /var/www/html/4.ISOs/ubuntu-16.04.1-desktop-amd64.iso /mnt/loop
+# cp /mnt/loop/casper/vmlinuz.efi /var/lib/tftpboot/ubuntu-16.04.1-desktop-amd64
+# cp /mnt/loop/casper/initrd.lz /var/lib/tftpboot/ubuntu-16.04.1-desktop-amd64
+# cp -R /mnt/loop/* /srv/install/ubuntu-16.04.1-desktop-amd64
+# cp -R /mnt/loop/.disk /srv/install/ubuntu-16.04.1-desktop-amd64
+# umount /mnt/loop
 
 ######### Syslinux setup stuff #####################################################
-mkdir -p /var/lib/tftpboot/pxelinux.cfg
+# mkdir -p /var/lib/tftpboot/pxelinux.cfg
 # wget https://help.ubuntu.com/community/PXEInstallMultiDistro?action=AttachFile&do=view&target=logo.png
-cp /home/user/Downloads/EAF_PXE-master/logo.png /var/lib/tftpboot/pxelinux.cfg
+# cp /home/user/Downloads/EAF_PXE-master/logo.png /var/lib/tftpboot/pxelinux.cfg
 # cp /usr/lib/syslinux/pxelinux.0 /var/lib/tftpboot
 # cp /usr/lib/syslinux/vesamenu.c32 /var/lib/tftpboot
-cp /usr/lib/syslinux/modules/bios/pxelinux.0 /var/lib/tftpboot
-cp /usr/lib/syslinux/modules/bios/vesamenu.c32 /var/lib/tftpboot
-touch /var/lib/tftpboot/pxelinux.cfg/pxe.conf
-touch /var/lib/tftpboot/pxelinux.cfg/default
-*/
+# cp /usr/lib/syslinux/modules/bios/pxelinux.0 /var/lib/tftpboot
+# cp /usr/lib/syslinux/modules/bios/vesamenu.c32 /var/lib/tftpboot
+# touch /var/lib/tftpboot/pxelinux.cfg/pxe.conf
+# touch /var/lib/tftpboot/pxelinux.cfg/default
+
 
 echo "/srv/install         10.10.10.0/24(rw,async,no_root_squash,no_subtree_check) " > /etc/exports
 exportfs -a
 /*
 # /var/lib/tftpboot/pxelinux.cfg/pxe.conf ############################################
-cat <<EOF_pxe.conf >> /var/lib/tftpboot/pxelinux.cfg/pxe.conf
-MENU TITLE  PXE Server
-MENU BACKGROUND pxelinux.cfg/logo.png
-NOESCAPE 1
-ALLOWOPTIONS 1
-PROMPT 0
-menu width 80
-menu rows 14
-MENU TABMSGROW 24
-MENU MARGIN 10
-menu color border               30;44      #ffffffff #00000000 std
-EOF_pxe.conf
+# cat <<EOF_pxe.conf >> /var/lib/tftpboot/pxelinux.cfg/pxe.conf
+# MENU TITLE  PXE Server
+# MENU BACKGROUND pxelinux.cfg/logo.png
+# NOESCAPE 1
+# ALLOWOPTIONS 1
+# PROMPT 0
+# menu width 80
+# menu rows 14
+# MENU TABMSGROW 24
+# MENU MARGIN 10
+# menu color border               30;44      #ffffffff #00000000 std
+# EOF_pxe.conf
 
 # /var/lib/tftpboot/pxelinux.cfg/default ##################################################
-cat <<EOF_default >> /var/lib/tftpboot/pxelinux.cfg/default
-DEFAULT vesamenu.c32
-TIMEOUT 600
-ONTIMEOUT BootLocal
-PROMPT 0
-MENU INCLUDE pxelinux.cfg/pxe.conf
-NOESCAPE 1
-LABEL Boot off internal drive
-      localboot 0
-      TEXT HELP
-      Boot to local hard disk
-      ENDTEXT
-LABEL 1
-      MENU LABEL DBAN - Hard Drive Sanitation
-      KERNEL dban-2.3.0_i586/dban.bzi
-      APPEND nuke=\"dwipe --autonuke\" silent vga=785
-      TEXT HELP
-      Warning - This will erase your hard drive
-      ENDTEXT
-/*
+# cat <<EOF_default >> /var/lib/tftpboot/pxelinux.cfg/default
+# DEFAULT vesamenu.c32
+# TIMEOUT 600
+# ONTIMEOUT BootLocal
+# PROMPT 0
+# MENU INCLUDE pxelinux.cfg/pxe.conf
+# NOESCAPE 1
+# LABEL Boot off internal drive
+#      localboot 0
+#      TEXT HELP
+#      Boot to local hard disk
+#      ENDTEXT
+# LABEL 1
+#      MENU LABEL DBAN - Hard Drive Sanitation
+#      KERNEL dban-2.3.0_i586/dban.bzi
+#      APPEND nuke=\"dwipe --autonuke\" silent vga=785
+#      TEXT HELP
+#      Warning - This will erase your hard drive
+#      ENDTEXT
+
 # echo "LABEL 2" >> /var/lib/tftpboot/pxelinux.cfg/default
 # echo "        MENU LABEL Inventory Machine - Alpha (not working)" >> /var/lib/tftpboot/pxelinux.cfg/default
 # echo "        KERNEL clonezilla-live-2.4.2-10-i586.iso/vmlinuz" >> /var/lib/tftpboot/pxelinux.cfg/default
@@ -221,30 +221,29 @@ LABEL 1
 # echo "        TEXT HELP" >> /var/lib/tftpboot/pxelinux.cfg/default
 # echo "        Boot the Inventory Machine" >> /var/lib/tftpboot/pxelinux.cfg/default
 # echo "        ENDTEXT" >> /var/lib/tftpboot/pxelinux.cfg/default
-*/
-LABEL 3
-       MENU LABEL List Images
-       KERNEL clonezilla-live-2.4.7-8-amd64/vmlinuz
-       APPEND initrd=clonezilla-live-2.4.7-8-amd64/initrd.img boot=live config noswap nolocales edd=on nomodeset noprompt ocs_prerun=\"mount -t cifs -o user=user,password=password //10.10.1.10/Images /home/partimag\" ocs_live_run=\"ocs-sr -g auto -e1 auto -e2 -batch -icds -r -j2 -k1 -p reboot restoredisk ask_user sda\" ocs_live_keymap=\"NONE\" ocs_live_batch=\"yes\" ocs_lang=\"en_US.UTF-8\" vga=791 ip=frommedia nosplash i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.blacklist=yes fetch=tftp://10.10.1.10/clonezilla-live-2.4.7-8-amd64/filesystem.squashfs
-       TEXT HELP
-       Boot the List Images
-       ENDTEXT
-LABEL 4
-       MENU LABEL Create Image
-       KERNEL clonezilla-live-2.4.2-10-i586/vmlinuz
-       APPEND initrd=clonezilla-live-2.4.2-10-i586/initrd.img boot=live config noswap nolocales edd=on nomodeset noprompt ocs_prerun=\"mount -t cifs -o user=user,password=password //10.10.1.10/Images /home/partimag\" ocs_live_run=\"ocs-sr -q2 -j2 -rm-win-swap-hib -z1 -i 2000 -sc -fsck-src-part-y -p true savedisk ask_user sda\" ocs_live_keymap=\"NONE\" ocs_live_batch=\"yes\" ocs_lang=\"en_US.UTF-8\" vga=791 ip=frommedia nosplash i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.blacklist=yes fetch=tftp://10.10.1.10/clonezilla-live-2.4.7-8-amd64/filesystem.squashfs
-       TEXT HELP
-       Boot the Create Image
-       ENDTEXT
-LABEL 5
-       MENU LABEL Ubuntu-16.04.1-desktop-amd64
-       KERNEL ubuntu-16.04.1-desktop-amd64/vmlinuz
-       APPEND boot=casper netboot=nfs nfsroot=10.10.1.10:/srv/install/ubuntu-16.04.1-desktop-amd64 initrd=ubuntu-16.04.1-desktop-amd64/initrd.lz
-       TEXT HELP
-       Boot the ubuntu-16.04.1-desktop-amd64
-       ENDTEXT
-EOF_default
-*/
+
+# LABEL 3
+#       MENU LABEL List Images
+#       KERNEL clonezilla-live-2.4.7-8-amd64/vmlinuz
+#       APPEND initrd=clonezilla-live-2.4.7-8-amd64/initrd.img boot=live config noswap nolocales edd=on nomodeset noprompt ocs_prerun=\"mount -t cifs -o user=user,password=password //10.10.1.10/Images /home/partimag\" ocs_live_run=\"ocs-sr -g auto -e1 auto -e2 -batch -icds -r -j2 -k1 -p reboot restoredisk ask_user sda\" ocs_live_keymap=\"NONE\" ocs_live_batch=\"yes\" ocs_lang=\"en_US.UTF-8\" vga=791 ip=frommedia nosplash i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.blacklist=yes fetch=tftp://10.10.1.10/clonezilla-live-2.4.7-8-amd64/filesystem.squashfs
+#       TEXT HELP
+#       Boot the List Images
+#       ENDTEXT
+# LABEL 4
+#       MENU LABEL Create Image
+#       KERNEL clonezilla-live-2.4.2-10-i586/vmlinuz
+#       APPEND initrd=clonezilla-live-2.4.2-10-i586/initrd.img boot=live config noswap nolocales edd=on nomodeset noprompt ocs_prerun=\"mount -t cifs -o user=user,password=password //10.10.1.10/Images /home/partimag\" ocs_live_run=\"ocs-sr -q2 -j2 -rm-win-swap-hib -z1 -i 2000 -sc -fsck-src-part-y -p true savedisk ask_user sda\" ocs_live_keymap=\"NONE\" ocs_live_batch=\"yes\" ocs_lang=\"en_US.UTF-8\" vga=791 ip=frommedia nosplash i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.blacklist=yes fetch=tftp://10.10.1.10/clonezilla-live-2.4.7-8-amd64/filesystem.squashfs
+#       TEXT HELP
+#       Boot the Create Image
+#       ENDTEXT
+# LABEL 5
+#       MENU LABEL Ubuntu-16.04.1-desktop-amd64
+#       KERNEL ubuntu-16.04.1-desktop-amd64/vmlinuz
+#       APPEND boot=casper netboot=nfs nfsroot=10.10.1.10:/srv/install/ubuntu-16.04.1-desktop-amd64 initrd=ubuntu-16.04.1-desktop-amd64/initrd.lz
+#       TEXT HELP
+#       Boot the ubuntu-16.04.1-desktop-amd64
+#       ENDTEXT
+# EOF_default
 
 chmod 777 -R /var/lib/tftpboot
 echo "****************************************************************************"
