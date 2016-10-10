d4f105b1b69e2e7c901eac9c3876cce293ce7fb2
techmore <sdolbec1@gmail.com>
2016-10-10 14:26:28 -0400

    Update add_windows.sh

--- add_windows.sh
+++ add_windows.sh
@@ -1,203 +1,58 @@
 #!/bin/bash
 
-#  /srv/install 23GB
-echo "This script will install x86 / x64 versions of the following operating systems. 
- Edu buntu - A complete Linux based operating system targeted for primary and secondary education. It is freely available with community based support. The Edubuntu community is built on the ideas enshrined in the Edubuntu Manifesto: that software, especially for education, should be available free of charge and that software tools should be usable by people in their local language and despite any disabilities.
-Kubuntu - An official derivative of Ubuntu Linux using KDE instead of the GNOME or Unity interfaces used by default in Ubuntu.
- Net Installer
- Ubuntu Desktop 
- Ubuntu Server - An official derivative made for use in servers. Ubuntu Server handles mail, controls printers, acts as a fileserver, can host LAMP and more.
- Ubuntu GNOME - An official Ubuntu variant[56] that uses Gnome Shell as its default desktop and GDM as its display manager.
- Ubuntu MATE - An official derivative of Ubuntu using MATE, a desktop environment forked from the now-defunct GNOME 2 code base, with an emphasis on the desktop metaphor.
- Lubuntu - Lubuntu is a project that is an official derivative of the Ubuntu operating system that is lighter, less resource hungry and more energy-efficient, using the LXDE desktop environment.
- Xubuntu - An official derivative of Ubuntu using Xfce. Xubuntu is intended for use on less-powerful computers or those who seek a highly efficient desktop environment on faster systems, and uses mostly GTK+ applications.
- Mythbuntu - Based on Ubuntu and MythTV, providing applications for recording TV and acting as a media center.
-  Ubuntu Studio - Based on Ubuntu, providing open-source applications for multimedia creation aimed at the audio, video and graphic editors. "
+[install]
+comment = Windows 7 Image
+path = /windows
+read only = no
+browseable = yes
+public = yes
+printable = no
+guest ok = yes
+oplocks = no
+level2 oplocks = no
+locking = no
 
-ubuntu_array=(
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-amd64.iso
-)
-ubuntu_net_array=(
-    http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-i386/current/images/netboot/netboot.tar.gz
-    http://archive.ubuntu.com/ubuntu/dists/trusty-updates/main/installer-amd64/current/images/netboot/netboot.tar.gz
-)
-iso_array=(
-   http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-amd64.iso 
-   http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-i386.iso 
-     
-   http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-i386.iso
-   http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-amd64.iso
+# Windows 7 64-bit DVD Image, but this time copy DVD mounted content to /windows/x64/ shared path.
 
-   http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-amd64.iso
-    
-   http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-amd64.iso
+# mount -o loop /dev/cdrom /mnt
+# cp -rf  /mnt/*  /windows/x64/
+# umount  /mnt
+chmod -R 0755 /windows
+# chown -R nobody:nobody /windows
 
-   http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-amd64.iso
+mkdir /var/lib/tftpboot/windows
 
-   http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-i386.iso
-   http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-amd64.iso
-     
-   http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-i386.iso
-   http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-amd64.iso
-)
+nano /var/lib/tftpboot/pxelinux.cfg/default
+label 9
+menu label ^9) Install Windows 7 x32/x64
+KERNEL memdisk
+INITRD windows/winpe_x86.iso
+APPEND iso raw
 
-torrent_array=(
-    http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-amd64.iso.torrent
-    http://cdimage.ubuntu.com/edubuntu/releases/14.04.2/release/edubuntu-14.04.3-dvd-i386.iso.torrent
-    
-    http://cdimage.ubuntu.com/kubuntu/releases/trusty/release/kubuntu-14.04.3-desktop-i386.iso.torrent
-    http://cdimage.ubuntu.com/kubuntu/releases/trusty/release/kubuntu-14.04.3-desktop-amd64.iso.torrent
+ON A WINDOWS 7 machine
+# Download windows 7 AIK
+http://www.microsoft.com/en-us/download/details.aspx?id=5753
 
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-i386.iso.torrent
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-desktop-amd64.iso.torrent
- 
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-i386.iso.torrent
-    http://releases.ubuntu.com/14.04.3/ubuntu-14.04.3-server-amd64.iso.torrent
+Daemon Tools Lite Free Edition
 
-    http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-i386.iso.torrent
-    http://cdimage.ubuntu.com/ubuntu-gnome/releases/14.04.3/release/ubuntu-gnome-14.04.3-desktop-amd64.iso.torrent
+3. After Windows AIK software is installed on your system go to Windows Start -> All Programs -> Microsoft Windows AIK -> right click on Deployment Tools Command Prompt and select Run as Administrator and a new Windows Shell console should open on your screen.
 
-     http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-i386.iso.torrent
-     http://cdimage.ubuntu.com/ubuntu-mate/releases/15.04/release/ubuntu-mate-15.04-desktop-amd64.iso.torrent
+copype amd64 C:\winPE_amd64
+copy "C:\Program Files\Windows AIK\Tools\PETools\amd64\winpe.wim" C:\winpe_amd64\ISO\Sources\Boot.wim
+copy "C:\Program Files\Windows AIK\Tools\amd64\Imagex.exe" C:\winpe_amd64\ISO\
+oscdimg -n -bC:\winpe_amd64\etfsboot.com C:\winpe_amd64\ISO C:\winpe_amd64\winpe_amd64.iso
 
-     http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-i386.iso.torrent
-     http://cdimage.ubuntu.com/lubuntu/releases/14.04/release/lubuntu-14.04.3-desktop-amd64.iso.torrent
+# Windows 8 build
+copype amd64 C:\Win8PE_amd64
+MakeWinPEMedia /ISO C:\Win8PE_amd64 C:\Win8PE_amd64\Win8PE_amd64.iso
 
-    http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-i386.iso.torrent
-    http://cdimage.ubuntu.com/xubuntu/releases/14.04.3/release/xubuntu-14.04.3-desktop-amd64.iso.torrent
+7. After WinPE x86 ISO file is completely transferred to Samba “install” shared directory go back to PXE Server console and move this image from root’s /windows directory to TFTP windows directory path to complete the entire installation process.
 
-     http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-i386.iso.torrent
-     http://cdimage.ubuntu.com/mythbuntu/releases/14.04.3/release/mythbuntu-14.04.3-desktop-amd64.iso.torrent
-
-     http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-i386.iso.torrent
-     http://cdimage.ubuntu.com/ubuntustudio/releases/14.04.3/release/ubuntustudio-14.04.3-dvd-amd64.iso.torrent
-)
+# mv /windows/winpe_x86.iso  /var/lib/tftpboot/windows/
 
 
-# echo "Please select : 
-# 1.ISO
-# 2.Torrent ( not working )"
+net use y : \\192.168.1.20\install\x64
+Y:
+setup.exe
 
-# read method
-
-# if [ method == 1 ]; then
-#    echo "Downloading ISOs"
-# fi
-
-# " >> /var/lib/tftpboot/pxelinux.cfg/pxe.conf
-
-echo " MENU BEGIN Ubuntu" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo " MENU TITLE Ubuntu" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         LABEL Previous" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         MENU LABEL Previous Menu" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         TEXT HELP" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         Return to previous menu" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         ENDTEXT" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         MENU EXIT" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         MENU SEPARATOR" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo "         MENU INCLUDE Ubuntu/Ubuntu.menu" >> /var/lib/tftpboot/pxelinux.cfg/default
-echo " MENU END" >> /var/lib/tftpboot/pxelinux.cfg/default
-
-
-sudo mkdir /var/lib/tftpboot/Ubuntu
-sudo touch /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-
-for x in ${ubuntu_array[@]}; do
-   # echo $x
-   y=$(echo $x | cut -d / -f 5)
-   z=$(echo $y | sed "s/.iso//g")
-
-#   echo $y
-   echo ""
-   if [ ! -f ~/Downloads/$y ]; then
-      echo "~/$y NOT found, attempting to download."
-      echo "--Downloading $y LTS..."
-      cd ~/Downloads
-      wget $x
-   else
-      echo "$y found."
-   fi
-   if [ ! -d /var/lib/tftpboot/Ubuntu/$z ]; then
-      sudo mkdir /var/lib/tftpboot/Ubuntu/$z; fi
-      sudo mount -o loop -t iso9660 ~/Downloads/$y /mnt/loop
-      sudo cp /mnt/loop/casper/vmlinuz /var/lib/tftpboot/Ubuntu/$z
-      sudo cp /mnt/loop/install/vmlinuz.efi /var/lib/tftpboot/Ubuntu/$z
-
-      sudo cp /mnt/loop/casper/initrd.lz /var/lib/tftpboot/Ubuntu/$z
-      sudo cp /mnt/loop/install/initrd.gz /var/lib/tftpboot/Ubuntu/$z
-
-      sudo mkdir -p /srv/install/Ubuntu/$z
-      sudo cp -R /mnt/loop/* /srv/install/Ubuntu/$z
-      sudo cp -R /mnt/loop/.disk /srv/install/Ubuntu/$z
-      sudo umount /mnt/loop
-done
-#
-# for x in ${ubuntu_net_array[@]}; do
-   # echo $x
-#   y=$(echo "net_installer_"; echo $x | cut -d / -f 8)
-#   echo $y
-#   echo ""
-#   if [ ! -f ~/Downloads/$y ]; then
-#      echo "ubuntu/$y NOT found, attempting to download."
-#      echo "--Downloading $y LTS..."
-#      cd ~/Downloads
-#     wget $x
-#   else
-#      echo "$y found."
-#   if [ ! - d /var/lib/tftpboot/ubuntu-14.04.3-desktop-i386 ]; then
-#     sudo mkdir /var/lib/tftpboot/ubuntu-14.04.3-desktop-i386; fi
-#     sudo mount -o loop -t iso9660 ~/Downloads/ubuntu-14.04.3-desktop-i386.iso /mnt/loop
-#     sudo cp /mnt/loop/casper/vmlinuz /var/lib/tftpboot/ubuntu-14.04.3-desktop-i386
-#     sudo cp /mnt/loop/casper/initrd.lz /var/lib/tftpboot/ubuntu-14.04.3-desktop-i3863
-
- #    sudo mkdir -p /srv/install/ubuntu-14.04.3-desktop-i386
- #     sudo cp -R /mnt/loop/* /srv/install/ubuntu-14.04.3-desktop-i386
- #     sudo cp -R /mnt/loop/.disk /srv/install/ubuntu-14.04.3-desktop-i386
- #     sudo umount /mnt/loop
- #  fi
-#done
-
-for x in ${iso_array[@]}; do
-   # echo $x
-   y=$(echo $x | cut -d / -f 8)
-   z=$(echo $y | sed "s/.iso//g")
-#   echo $y
- #  echo $z
-   echo ""
-   if [ ! -f ~/Downloads/$y ]; then
-      echo "~/Downloads/$y NOT found, attempting to download."
-      echo "--Downloading $y LTS..."
-      cd ~/Downloads
-      wget $x
-   else
-      echo "$y found."
-   fi
-   if [ ! -d /var/lib/tftpboot/Ubuntu/$z ]; then
-     sudo mkdir -v "/var/lib/tftpboot/Ubuntu/$z"
-     sudo mount -o loop -t iso9660 ~/Downloads/$y /mnt/loop
-     sudo cp /mnt/loop/casper/vmlinuz "/var/lib/tftpboot/Ubuntu/$z"
-     sudo cp /mnt/loop/casper/vmlinuz.efi "/var/lib/tftpboot/Ubuntu/$z"
-     sudo cp /mnt/loop/casper/initrd.lz "/var/lib/tftpboot/Ubuntu/$z"
-
-     if [ ! -d /srv/install/$z ]; then
-       sudo mkdir -p -v /srv/install/$z
-       sudo cp -R -v /mnt/loop/* /srv/install/$z
-       sudo cp -R -v /mnt/loop/.disk /srv/install/$z
-     fi
-     sudo umount /mnt/loop
-     
-     echo "LABEL" >> /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-     echo "        MENU LABEL $z" >> /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-     echo "        KERNEL Ubuntu/$z/vmlinuz.efi" >> /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-     echo "        APPEND boot=casper netboot=nfs nfsroot=10.10.1.10:/srv/install/Ubuntu/$z initrd=Ubuntu/$z/initrd.lz" >> /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-     echo "        TEXT HELP" >> /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-     echo "        Boot the $z" >> /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-     echo "        ENDTEXT" >> /var/lib/tftpboot/Ubuntu/Ubuntu.menu
-   fi
-done
-
+net use y : \\192.168.1.20\install\x64  /user:samba_username
