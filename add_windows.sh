2927d107e4ba47dfa7f3ebc0aa74787ffed9947f
techmore <sdolbec1@gmail.com>
2016-10-10 15:22:59 -0400

    Update add_windows.sh

--- add_windows.sh
+++ add_windows.sh
@@ -3,6 +3,14 @@
 
 echo "This program expects Windows_7_x64.iso, WinPE_x64.iso in $HOME/Downloads folder."
 
+# cd /var/www/html/4.ISOs
+# This program will download Windows AIK for windows 7 and put it in 4.ISOs on the PXE server."
+# wget https://download.microsoft.com/download/8/E/9/8E9BBC64-E6F8-457C-9B8D-F6C9A16E6D6A/KB3AIK_EN.iso
+
+# If you want to build your windows PE here download Virtualbox
+# http://download.virtualbox.org/virtualbox/5.1.6/virtualbox-5.1_5.1.6-110634~Ubuntu~xenial_amd64.deb
+# dpkg -i virtualbox-5.1_5.1.6-110634~Ubuntu~xenial_amd64.deb
+
 mkdir -p /srv/windows
 cat <<EOF >> /etc/samba/smb.conf
   [Windows_install]
