921477144d3e435628e5e953b7459748e67d54fe
techmore <sdolbec1@gmail.com>
2016-10-10 17:43:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -164,6 +164,8 @@ cp /var/www/html/4.ISOs/netboot/ubuntu-installer/amd64/boot-screens/libutil.c32 
 # cp /usr/lib/syslinux/vesamenu.c32 /var/lib/tftpboot
 # cp /usr/lib/syslinux/modules/bios/pxelinux.0 /var/lib/tftpboot
 # cp /usr/lib/syslinux/modules/bios/vesamenu.c32 /var/lib/tftpboot
+apt -y install syslinux
+cp /usr/lib/syslinux/memdisk /var/lib/tftpboot
 
 chown -R nobody:nogroup /var/lib/tftpboot
 
@@ -247,6 +249,9 @@ cat <<EOF_default >> /var/lib/tftpboot/pxelinux.cfg/default
         MENU LABEL Create Image
         KERNEL clonezilla-live-2.4.7-8-amd64/vmlinuz
         APPEND initrd=clonezilla-live-2.4.7-8-amd64/initrd.img boot=live config noswap nolocales edd=on nomodeset noprompt ocs_prerun=\"mount -t cifs -o user=user,password=password //10.10.10.10/Images /home/partimag\" ocs_live_run=\"ocs-sr -q2 -j2 -rm-win-swap-hib -z1 -i 2000 -sc -fsck-src-part-y -p true savedisk ask_user sda\" ocs_live_keymap=\"NONE\" ocs_live_batch=\"yes\" ocs_lang=\"en_US.UTF-8\" vga=791 ip=frommedia nosplash i915.blacklist=yes radeonhd.blacklist=yes nouveau.blacklist=yes vmwgfx.blacklist=yes fetch=tftp://10.10.10.10/clonezilla-live-2.4.7-8-amd64/filesystem.squashfs
+# Preseed from website
+# append auto=true priority=critical vga=788 initrd=ubuntu-installer/amd64/initrd.gz preseed/url=tftp://192.168.11.66/preseed/ubuntu-16.04-preseed.cfg preseed/interactive=false
+
         TEXT HELP
         Boot the Create Image
         ENDTEXT
@@ -258,6 +263,49 @@ cat <<EOF_default >> /var/lib/tftpboot/pxelinux.cfg/default
         Boot the ubuntu-16.04.1-desktop-amd64
         ENDTEXT
 EOF_default
+
+# Preseed file for Ubuntu 16.04.1
+
+# cat << EOF_Preseed >>  /var/lib/tftpboot/preseed/ubuntu-16.04-preseed.cfg
+#  d-i debian-installer/locale string en_US
+#  d-i debian-installer/language string en
+#  d-i debian-installer/country string JP
+#  d-i keyboard-configuration/xkb-keymap select jp106
+#  d-i passwd/user-fullname string
+#  d-i passwd/username string ubuntu
+#  d-i passwd/root-password password ubuntu
+#  d-i passwd/root-password-again password ubuntu
+#  d-i passwd/user-password password ubuntu
+#  d-i passwd/user-password-again password ubuntu
+#  d-i user-setup/allow-password-weak boolean true
+#  d-i netcfg/choose_interface select auto
+#  d-i netcfg/get_hostname string unassigned-hostname
+#  d-i netcfg/get_domain string unassigned-domain
+#  d-i mirror/country string manual
+#  d-i mirror/http/hostname string http://jp.archive.ubuntu.com
+#  d-i mirror/http/directory string /ubuntu
+#  d-i mirror/http/proxy string
+#  d-i clock-setup/utc boolean true
+#  d-i clock-setup/ntp boolean true
+#  d-i time/zone string Asia/Tokyo
+#  d-i partman/confirm boolean true
+#  d-i partman/choose_partition select finish
+#  d-i partman/confirm_nooverwrite boolean true
+#  d-i partman-auto/disk string /dev/[sv]da
+#  d-i partman-auto/method string lvm
+#  d-i partman-auto/choose_recipe select atomic
+#  d-i partman-lvm/device_remove_lvm boolean true
+#  d-i partman-lvm/confirm boolean true
+#  d-i partman-lvm/confirm_nooverwrite boolean true
+#  d-i partman-auto-lvm/guided_size string max
+#  d-i partman-partitioning/confirm_write_new_label boolean true
+#  d-i grub-installer/grub2_instead_of_grub_legacy boolean true
+#  d-i grub-installer/only_debian boolean true
+#  d-i grub-installer/bootdev string /dev/[sv]da
+#  d-i pkgsel/update-policy select none
+#  d-i pkgsel/include string unity ubuntu-desktop openssh-server
+#  d-i finish-install/reboot_in_progress note
+# EOF_Preseed
 
 chmod 777 -R /var/lib/tftpboot
 echo "****************************************************************************"
