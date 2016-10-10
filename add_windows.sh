354275f4827f540d32cfddb78551b268cf21f01b
techmore <sdolbec1@gmail.com>
2016-10-10 14:57:48 -0400

    Update add_windows.sh

--- add_windows.sh
+++ add_windows.sh
@@ -39,23 +39,16 @@ cat << EOF_pxelinux.cfg >> /var/lib/tftpboot/pxelinux.cfg/default
   APPEND iso raw
 EOF_pxelinux.cfg
 
+cat << EOF_How >> /var/www/html/3.Scripts/How_to_install_Windows.html
+<pre>
+ Run this on the PE booting machine.
 
-# ON A WINDOWS 7 machine
-# Download windows 7 AIK
-# http://www.microsoft.com/en-us/download/details.aspx?id=5753
-# Daemon Tools Lite Free Edition
-# 3. After Windows AIK software is installed on your system go to Windows Start -> All Programs -> Microsoft Windows AIK -> right click on Deployment Tools Command Prompt and select Run as Administrator and a new Windows Shell console should open on your screen.
+ net use z : \\10.10.10.10\x64
+ Y:
+ setup.exe
 
-# copype amd64 C:\winPE_amd64
-# copy "C:\Program Files\Windows AIK\Tools\PETools\amd64\winpe.wim" C:\winpe_amd64\ISO\Sources\Boot.wim
-# copy "C:\Program Files\Windows AIK\Tools\amd64\Imagex.exe" C:\winpe_amd64\ISO\
-# oscdimg -n -bC:\winpe_amd64\etfsboot.com C:\winpe_amd64\ISO C:\winpe_amd64\winpe_amd64.iso
-# 7. After WinPE x86 ISO file is completely transferred to Samba “install” shared directory go back to PXE Server console and move this image from root’s /windows directory to TFTP windows directory path to complete the entire installation process.
+ For authentication
+ net use z : \\10.10.10.10\x64  /user:samba_username
+</pre>
+EOF_How
 
-# Run this on the PE booting machine.
-
-# net use y : \\192.168.1.20\install\x64
-# Y:
-# setup.exe
-
-# net use y : \\192.168.1.20\install\x64  /user:samba_username
